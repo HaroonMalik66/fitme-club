4da119ec1c0fa238997c8e8293a024d81dc6f8e0
Harooon Ahmed <haroonmalik4219855@gmail.com>
2022-10-25 08:22:57 +0500

    add new logo and some tweaks

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -6,20 +6,22 @@ export const testimonialsData = [
   {
     image: image1,
     review:
-      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+      "I made the right choice by choosing the Fit Me club and by choosing the right plan and program I already achieved my ideal body!",
+    name: "MATHEW HENDRICKSON",
+    status: "ENTREPRENEUR",
   },
   {
     image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
+    review:
+      "This is the best place I ever served. I can say without doubt that Fit Me Club is one of the finest clubs in the country ",
+    name: "JOHN KEVIN",
+    status: "COACH",
   },
   {
-    image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
-  }
+    image: image3,
+    review:
+      " When I first joined, I had doubts but man they are so professional and provide the best services. 100% satisfied",
+    name: "FRANKLIN",
+    status: "CUSTOMER",
+  },
 ];
